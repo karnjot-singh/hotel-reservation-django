be0488169a17c9b5fc468b75e36a77dd9bc25fcd
Karnjot Singh <karnjot.singh@smu.ca>
2022-03-01 22:25:02 -0400

    config changed

--- hotelreservation/settings.py
+++ hotelreservation/settings.py
@@ -77,14 +77,8 @@ WSGI_APPLICATION = 'hotelreservation.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'k_singh',
-        'USER': 'k_singh',
-        'PASSWORD': 'A00457246!cda',
-        'HOST': '127.0.0.1',
-        'PORT': '8005',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
